5bd5701ce5a37c6c65a394d41ff13f0d7c78d640
xsl1009 <xsl1009@hotmail.com>
2018-03-21 16:09:11 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme. I love you puppet. Oh, Finally figure out LOLOLOLOLOL\n',
-    owner   => 'root',
-    }
-  file {'/root/README':
-    owner   => 'root',
-  }
 }
